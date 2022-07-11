9b6a727a9be2b46211d1203b54fb205fbd3874a9
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-11 15:54:22 -0300

    repository# DbSets for Travel

---
+++ Repository/TravelContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Domain;
+
+namespace Repository
+{
+    public class TravelContext : DbContext
+    {
+        #region Entities
+        public DbSet<Ciudad> Ciudades { get; set; }
+        public DbSet<EstadoViaje> EstadoViajes { get; set; }
+        public DbSet<Pais> Paises { get; set; }
+        public DbSet<Pronostico> Pronosticos { get; set; }
+        public DbSet<ReprogramacionViaje> ReprogramacionViajes { get; set; }
+        public DbSet<TipoVehiculo> TipoVehiculos { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+        public DbSet<Viaje> Viajes { get; set; }
+        #endregion
+
+        public TravelContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
