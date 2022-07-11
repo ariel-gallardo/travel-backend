779cf66b4f04be9385f3a2534fc2553ea7a9ff85
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-11 15:49:01 -0300

    models# domain base model & output base model

---
+++ Models/Domain/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Domain
+{
+    public class Entity
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now.ToUniversalTime();
+        public DateTime? UptatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
