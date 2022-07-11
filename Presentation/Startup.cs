d4aa8d8943b4f75024d02174ee535530dcb03921
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-11 15:46:54 -0300

    dev# initial structure

---
+++ Presentation/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace Presentation
+{
+    public class Startup
+    {
+        #region props
+        private readonly IConfiguration _configuration;
+        #endregion props
+        public Startup(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.UseEndpoints(e => e.MapControllers());
+        }
+    }
+}
