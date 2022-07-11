ff354e948684736e6636551e003ecb8b451bdfd1
Ariel Gallardo <38444032+ariel-gallardo@users.noreply.github.com>
2022-07-11 15:53:06 -0300

    domain# base models for domain (#2)

---
+++ Models/Domain/Ciudad.cs
@@ -0,0 +1,9 @@
+﻿namespace Models.Domain
+{
+    public class Ciudad : Entity
+    {
+        public string Nombre { get; set; }
+        public long PaisId { get; set; }
+        public virtual Pais Pais { get; set; }
+    }
+}
---
+++ Models/Domain/Viaje.cs
@@ -0,0 +1,14 @@
+﻿namespace Models.Domain
+{
+    public class Viaje
+    {
+        public Pais PaisOrigen { get; set; }
+        public Pais PaisDestino { get; set; }
+        public Ciudad CiudadOrigen { get; set; }
+        public Ciudad CiudadDestino { get; set; }
+        public Vehiculo VehiculoAsignado { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public EstadoViaje Estado { get; set; }
+    }
+}
