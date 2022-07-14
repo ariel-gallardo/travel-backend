03b4669c5dad4a7f8221f88946b14d57685ad521
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-14 00:35:55 -0300

    abm/viajes# models

---
+++ Models/DataAnnotations/CustomDateAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace Services.Helpers
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    namespace Models.DataAnnotations
+    {
+        public class CustomDateAttribute : ValidationAttribute
+        {
+            public CustomDateAttribute()
+            {
+            }
+
+            public override bool IsValid(object value)
+            {
+                var dt = (DateTime)value;
+                var dtMax = dt.AddDays(10);
+
+                if (dt >= DateTime.Now && dt <= dtMax)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
--- Services/Helpers/Mapper.cs
+++ Services/Helpers/Mapper.cs
@@ -9,11 +9,21 @@ namespace Services
                 CreateMap<Models.Domain.Ciudad, Models.Output.Ciudad>()
                 .ForMember(o => o.Pais, opt => opt.MapFrom(d => d.Pais.Nombre));
                 CreateMap<Models.Domain.Pais, Models.Output.Pais>();
+
                 CreateMap<Models.Domain.TipoVehiculo, Models.Output.TipoVehiculo>();
                 CreateMap<Models.Domain.Vehiculo, Models.Output.Vehiculo>()
                 .ForMember(o => o.TipoVehiculo, opt => opt.MapFrom(d => d.Tipo.Denominacion));
+
+                CreateMap<Models.Domain.Viaje, Models.Output.Viaje>()
+                .ForMember(o => o.EstadoViaje, opt => opt.MapFrom(d => d.EstadoViaje.LastOrDefault()))
+                .ForMember(o => o.PaisOrigen, opt => opt.MapFrom(d => d.CiudadOrigen.Pais.Nombre))
+                .ForMember(o => o.PaisDestino, opt => opt.MapFrom(d => d.CiudadDestino.Pais.Nombre))
+                .ForMember(o => o.CiudadOrigen, opt => opt.MapFrom(d => d.CiudadOrigen.Nombre))
+                .ForMember(o => o.CiudadDestino, opt => opt.MapFrom(d => d.CiudadDestino.Nombre))
+                .ForMember(o => o.FechaInicio, opt => opt.MapFrom(d => d.FechaInicio.ToLocalTime().ToString()))
+                .ForMember(o => o.FechaFin, opt => opt.MapFrom(d => d.FechaFin.ToLocalTime().ToString()));
         }
-        }
+    }
 
         public class InputToDomain : Profile
         {
@@ -22,6 +32,7 @@ namespace Services
                 CreateMap<Models.Input.Ciudad, Models.Domain.Ciudad>();
                 CreateMap<Models.Input.Pais, Models.Domain.Pais>();
                 CreateMap<Models.Input.Vehiculo, Models.Domain.Vehiculo>();
+                CreateMap<Models.Input.Viaje, Models.Domain.Viaje>();
             }
         }
 }
