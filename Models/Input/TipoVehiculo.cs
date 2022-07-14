485d649c7e0c4de38a74c5d0cff8adfa30993aa4
Ariel Gallardo <ariel.gallardo96@gmail.com>
2022-07-13 21:21:23 -0300

    abm/vehiculos# Models for TipoVehiculo - Vehiculo

---
+++ Models/Input/TipoVehiculo.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.Input
+{
+    public class TipoVehiculo
+    {
+        [Required(ErrorMessage ="Falta la denominacion.")]
+        public string Denominacion { get; set; }
+    }
+}
